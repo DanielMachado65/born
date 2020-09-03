7f54bae968d343dc932a5b9f0b9350fc07446cc8
Daniel Machado <danielmachadopintos@gmail.com>
2020-09-02 22:10:30 -0300

    feat(menu): add menu

--- src/views/template/Menu.vue
+++ src/views/template/Menu.vue
@@ -1,12 +1,35 @@
 <template>
-  <aside class="menu">menu</aside>
+  <aside class="menu">
+    <div class="logo">
+      <v-img :src="logo" class="logo" width="60" alt="Logo" />
+    </div>
+    <div class="menu-itens">
+      <span class="mr-2">Logo</span>
+      <span class="mr-2">Sobre</span>
+      <span class="mr-2">Loja</span>
+      <span class="mr-2">Inscrição</span>
+    </div>
+  </aside>
 </template>
 
 <script>
+import logo from "../../assets/logo.png";
 export default {
   name: "Menu",
+  data: () => ({
+    logo,
+  }),
 };
 </script>
 
-<style>
+<style scoped>
+.menu {
+  display: flex;
+  justify-content: space-between;
+}
+
+.menu .menu-itens {
+  align-items: center;
+  display: flex;
+}
 </style>
